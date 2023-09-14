c61e011baa0084965349f2b763ade6df92f5e0d6
geffrys <74202340+geffrys@users.noreply.github.com>
2023-09-14 17:05:38 -0500

    Updating
    Adapting software interruptions into the project

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,63 @@
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
         MemorySpace memorySpace = new MemorySpace();
-        String process1 = "process1";
-        System.out.println("Process 1 created");
-        String process2 = "process2";
-        System.out.println("Process 2 created");
+        String process1 = "process 1";
+        System.out.println("Process 1 created.");
+        String process2 = "process 2";
+        System.out.println("Process 2 created.");
         Thread thread1 = new Thread(()->{
             try {
+                Thread.sleep(2000);
                 memorySpace.enter(process1);
-            } catch (NotAuthorizedException | InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
         Thread thread2 = new Thread(()->{
             try {
                 memorySpace.enter(process2);
-            } catch (NotAuthorizedException | InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
+
+        // an alternative flow of interruption
+        Thread thread3 = new Thread(() -> {
+            int counter = 10;
+            while(counter >0 ){
+                System.out.println("cpu time: " + counter);
+                try{
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+                    System.out.println(e.getMessage());
+                }
+                counter--;
+            }
+            thread2.interrupt();
+        });
         thread1.start();
         thread2.start();
-        thread1.interrupt();
+        thread3.start();
+
     }
 }
 
 class MemorySpace {
-
     private boolean isAvailable;
     public MemorySpace() {
         isAvailable=true;
     }
-    public void enter(String process) throws NotAuthorizedException, InterruptedException {
+    public void enter(String process) throws  InterruptedException {
         if (!isAvailable) {
-            System.out.println(process + " is trying to enter critical section that is not available");
-            throw new NotAuthorizedException("Not authorized to enter critical section");
+            throw new InterruptedException(process+" are trying to enter critical section that is not available.");
         }
         if(Thread.interrupted()){
-            System.out.println(process + " is trying to enter critical section but was interrupted");
-            throw new InterruptedException("Interrupted while waiting to enter critical section");
+            isAvailable = true;
+            throw new InterruptedException("process interrupted");
         }
-        System.out.println(process + " entered critical section");
+        System.out.println(process + " has entered to critical section.");
         isAvailable = false;
-    }
-
-}
-
-class NotAuthorizedException extends Exception {
-    public NotAuthorizedException(String s) {
-        super("Not authorized to enter critical section");
     }
 }
 
